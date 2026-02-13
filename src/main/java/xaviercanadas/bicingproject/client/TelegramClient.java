9fb18604492b7302906bd378e730611639199ea8
XavierCanadas <xavier.canadas01@estudiant.upf.edu>
2026-02-13 14:04:52 +0100

    Add telegram token

---
+++ src/main/java/xaviercanadas/bicingproject/client/TelegramClient.java
@@ -0,0 +1,7 @@
+package xaviercanadas.bicingproject.client;
+
+public class TelegramClient {
+    public static void main(String[] args) {
+        String telegramToken = System.getenv("TOKEN_TELEGRAM");
+    }
+}
