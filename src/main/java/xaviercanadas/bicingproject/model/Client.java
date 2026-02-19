9f62f56b7cd368b5504a0db9395a3ce81a3054fb
XavierCanadas <xavier.canadas01@estudiant.upf.edu>
2026-02-19 14:29:08 +0100

    create client model

---
+++ src/main/java/xaviercanadas/bicingproject/model/Client.java
@@ -0,0 +1,61 @@
+package xaviercanadas.bicingproject.model;
+
+import java.util.List;
+
+public class Client {
+    private String phoneNumber;
+    private String telegramToken;
+    private long telegramChatId;
+    private List<Integer> stationIds;
+
+    public Client() {}
+
+    public Client(String phoneNumber, String telegramToken, long telegramChatId, List<Integer> stationIds) {
+        this.phoneNumber = phoneNumber;
+        this.telegramToken = telegramToken;
+        this.telegramChatId = telegramChatId;
+        this.stationIds = stationIds;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getTelegramToken() {
+        return telegramToken;
+    }
+
+    public void setTelegramToken(String telegramToken) {
+        this.telegramToken = telegramToken;
+    }
+
+    public long getTelegramChatId() {
+        return telegramChatId;
+    }
+
+    public void setTelegramChatId(long telegramChatId) {
+        this.telegramChatId = telegramChatId;
+    }
+
+    public List<Integer> getStationIds() {
+        return stationIds;
+    }
+
+    public void setStationIds(List<Integer> stationIds) {
+        this.stationIds = stationIds;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "phoneNumber: " + phoneNumber + '\n' +
+                ", telegramToken: " + telegramToken + '\n' +
+                ", telegramChatId: " + telegramChatId + '\n' +
+                ", stationIds: " + stationIds + '\n' +
+                '}';
+    }
+}
